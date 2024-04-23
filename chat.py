6f0359b174c0f6bb3e0db68ea3a3dc5f6f429ee9
Juan Rosales <jurosale3@gmail.com>
2024-04-22 17:03:18 -0700

    Initial commit

---
+++ chat.py
@@ -0,0 +1,195 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_core.output_parsers import BaseOutputParser
+from langchain_core.prompts.chat import ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+import create_pokemon_csv as PKMN_CSV
+
+
+ENDING_PHRASE = "Thank you for talking to me!"
+user_pokemon_data = {} # Need to make this global so parser class can access it
+
+
+class CustomParser(BaseOutputParser):
+    """
+    This parser object allows users to format a chat model's response.
+    """
+    def parse(self, text: str) -> str:
+        # Assume pokemon speaks normally unless speech value says otherwise
+        formatted_text = text
+        
+        # If pokemon speaks telepathically, showcase it
+        if user_pokemon_data[PKMN_CSV.CSV_SPEECH_KEY] == "telepathy":
+            formatted_text = "*Speaking telepathically* " + formatted_text
+
+        # Else if pokemon speaks through pokemon noises, showcase it
+        elif user_pokemon_data[PKMN_CSV.CSV_SPEECH_KEY] == "noise":
+            formatted_text = "*" + user_pokemon_data[PKMN_CSV.CSV_NAME_KEY] + " noises* (" + formatted_text + ")"
+
+        # Format should always be: [pokemon_name]([pokedex_id]): speech
+        return "\n" + user_pokemon_data[PKMN_CSV.CSV_NAME_KEY] + " (#" + user_pokemon_data[PKMN_CSV.CSV_ID_KEY] + "): " + formatted_text
+
+
+if __name__ == "__main__":
+    # Load up hidden API key located in a different
+    # file and set up chatbot model
+    load_dotenv()
+    api_key = os.getenv("OPENAI_API_KEY")
+    chat_model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=api_key)
+    
+    # Retrieve pokemon data from websites
+    pkmn_obj = PKMN_CSV.PokemonData()
+
+    # Assign prompt & input
+    template = """
+        You are {pokemon_name}, a {type} type pokemon from generation {gen}. You are talking to a user who wants to ask you questions about yourself or give you requests. Follow the conversation rules listed below when responding to the user.
+
+        Conversation Rules:
+        1. Keep your responses to 3 sentences max unless the user explicitly gives you a response length to follow.
+        2. If the user asks something inappropriate, reply with "I don't feel comfortable with this conversation. Would you like to talk about something else instead?".
+        3. If the user's input is incomprehensible, reply with "I don't understand. Would you like to ask me something else?".
+        4. If the user asks something that is not related to the pokemon world or you in any way, reply with "I don't know anything outside of the pokemon world. Would you like to ask me something else?".
+        5. If the user asks you something and you don't know the answer, reply with "I don't know. Would you like to ask me something else?".
+        6. Do not lie or make up answers. Use the information found in "Important Information" as additional context (if possible) when deciding on a response.
+        7. Use the conversation history in "Chat History" as additional context when evaluating the user's input.
+        8. If the conversation ends, the user declines to ask you more questions or "Current Response Count" is greater than 10, do not ask a question and append "{end_phrase}" to the end of your response.
+        
+        Current Response Count:
+        {count}
+        
+        Important Information:
+        {context}
+        
+        Chat History:
+        {chat_history}
+    """
+    human_template = "{text}"
+
+    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    print("\nPokeDex: Hello, I am a virtual pokedex. I can help you talk to your favorite pokemon!")
+
+    chat_history = []
+    retriever = ""
+    pkmn_typing = ""
+    response_count = 0
+    is_running = True
+    while True:
+        # Ensure a pokemon is selected to converse with
+        while not user_pokemon_data:
+            web_paths = [PKMN_CSV.PKMN_DB_WEB_PATH]
+            user_input = input("\nPokeDex: Please type in the pokemon's name or pokedex number to get started or type 'exit' if you're done.\n  -> ")
+            if not user_input:
+                continue
+            elif "exit" in user_input:
+                is_running = False
+                break
+            # If user (only) typed in a number, check for pokemon by pokedex id
+            elif user_input.isdigit():
+                int_val = int(user_input)
+                if int_val == 0 or int_val > pkmn_obj.max_pokemon:
+                    print(f"\nPokeDex: There is no such pokemon with that pokedex number. Try a number between 1 and {pkmn_obj.max_pokemon}.")
+                    continue
+                for line in pkmn_obj.csv_data:
+                    if int_val == int(line[PKMN_CSV.CSV_ID_KEY]):
+                        user_pokemon_data = line
+                        break
+            # Else, check for pokemon by name
+            else:
+                formatted_input = user_input.lower()
+                for line in pkmn_obj.csv_data:
+                    if formatted_input == line[PKMN_CSV.CSV_NAME_KEY].lower():
+                        user_pokemon_data = line
+                        break
+                if not user_pokemon_data:
+                    print("\nPokeDex: There is no such pokemon with that name.")
+
+            # Once user has selected a valid pokemon, retrieve their additional data
+            # and start up conversation with the chosen pokemon
+            if user_pokemon_data:
+                # Print this statement to buy some time while loading the chosen pokemon's web data
+                print(f"\nContacting {user_pokemon_data[PKMN_CSV.CSV_NAME_KEY]}...")
+                
+                # TODO: Look into and fix csv loading issue?
+                # The links list comes back as a string when uploading csv data unfortunately...fixing it here
+                formatted_links_list = user_pokemon_data[PKMN_CSV.CSV_LINKS_KEY][1:-1].replace("'","").split(", ")
+                web_paths.extend(formatted_links_list)
+
+                loader = WebBaseLoader(
+                    web_paths=(web_paths),
+                    requests_per_second=2
+                )
+                web_data = loader.load()
+
+                # Split data into chunks for easier lookup
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=1000, chunk_overlap=200, add_start_index=True
+                )
+                split_web_data = text_splitter.split_documents(web_data)
+
+                # Store in a vector database for easier querying
+                vectorstore = Chroma.from_documents(documents=split_web_data, embedding=OpenAIEmbeddings())
+
+                # Set the retriever and filtering parameters
+                retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 20})
+                
+                # TODO: Figure how out how to get OpenAI to consistently start with this intro so I don't need to hardcode it here!
+                # Always start with this Intro:
+                intro_text = f"Hello, my name is {user_pokemon_data[PKMN_CSV.CSV_NAME_KEY]}. I am a {user_pokemon_data[PKMN_CSV.CSV_TYPE_KEY]} type pokemon from gen {user_pokemon_data[PKMN_CSV.CSV_GEN_KEY]}. What would you like to ask me?"
+                
+                # If pokemon speaks telepathically, showcase it
+                if user_pokemon_data[PKMN_CSV.CSV_SPEECH_KEY] == "telepathy":
+                    intro_text = "*Speaking telepathically* " + intro_text
+                # Else if pokemon speaks through pokemon noises, showcase it
+                elif user_pokemon_data[PKMN_CSV.CSV_SPEECH_KEY] == "noise":
+                    intro_text = "*" + user_pokemon_data[PKMN_CSV.CSV_NAME_KEY] + " noises* (" + intro_text + ")"
+
+                print(f"\n{user_pokemon_data[PKMN_CSV.CSV_NAME_KEY]} (#{user_pokemon_data[PKMN_CSV.CSV_ID_KEY]}): {intro_text}")
+
+        if not is_running:
+            break
+
+        # Converse with user
+        user_input = ""
+        while not user_input:
+            user_input = input("  -> ")
+
+        # Insert prompt, input and formmatting into chat model and print the response
+        response_count += 1
+        chain = chat_prompt | chat_model | CustomParser()
+        result = chain.invoke(
+            {"pokemon_name":user_pokemon_data[PKMN_CSV.CSV_NAME_KEY],
+            "type":pkmn_typing,
+            "gen":user_pokemon_data[PKMN_CSV.CSV_GEN_KEY],
+            "text":user_input,
+            "end_phrase":ENDING_PHRASE,
+            "count":response_count,
+            "context":retriever,
+            "chat_history":"\n".join(chat_history)}
+        )
+        print(result)
+
+        # keep track of the 20 most recent inputs & responses
+        chat_history.append("User: " + user_input)
+        chat_history.append("You: " + result)
+        chat_history = chat_history[-20:]
+
+        # End current conversation and reset variables
+        if ENDING_PHRASE in result:
+            user_pokemon_data = {}
+            chat_history = []
+            retriever = ""
+            pkmn_typing = ""
+            response_count = 0
+
+    print("\nPokeDex: Understood, goodbye!")
---
+++ create_pokemon_csv.py
@@ -0,0 +1,253 @@
+import csv
+import os
+
+import bs4
+from langchain_community.document_loaders import WebBaseLoader
+
+
+DIR = os.path.dirname(__file__)
+CSV_FILE_PATH = os.path.join(DIR, "Pokemon.csv")
+VERSION_FILE_PATH = os.path.join(DIR, "Version.txt")
+
+CSV_ID_KEY = "ID"
+CSV_NAME_KEY = "Name"
+CSV_TYPE_KEY = "Type"
+CSV_SPEECH_KEY = "Speech"
+CSV_GEN_KEY = "Generation"
+CSV_LINKS_KEY = "Links"
+CSV_FIELDS =  [CSV_ID_KEY,CSV_NAME_KEY,CSV_TYPE_KEY,CSV_SPEECH_KEY,CSV_GEN_KEY,CSV_LINKS_KEY]
+
+POKEMON_TYPES = ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison",
+"ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"]
+
+PKMN_DB_WEB_PATH = "https://pokemondb.net/pokedex/national"
+PKMN_TALK_WEB_PATH = "https://pallettown.fandom.com/wiki/Talking_Pok%C3%A9mon"
+
+SPLIT_PKMN_LIST_STR = "<div class=\"infocard\"><span class=\"infocard-lg-img\">"
+SPLIT_PKMN_DATA_STR = "<a class=\"ent-name\" href=\"/pokedex/"
+SPLIT_PKMN_TYPE_STR = "<a class=\"itype "
+
+# Increment this everytime there are csv changes or updates from data websites
+CURRENT_VERSION = 1
+
+
+class PokemonData():
+    """
+    Object for holding all downloaded Pokemon data
+    """
+    csv_data: list[dict]
+    max_pokemon: int
+    _version: int
+
+    def __init__(self):
+        self.load()
+        if not self.is_current_version():
+            self.retrieve_and_write()
+
+    @property
+    def version(self) -> int:
+        return self._version
+    
+    @version.setter
+    def version(self, value: int):
+        self._version = value
+
+    def load(self):
+        """
+        Loads data from generated csv path. If it's missing or fails
+        for any reason, it will generate a new one
+        """
+        try:
+            with open(CSV_FILE_PATH, mode ='r') as f:
+                pokemon_count = 0
+                self.csv_data = []
+                for data in csv.DictReader(f):
+                    self.csv_data.append(data)
+                    pokemon_count += 1
+                self.max_pokemon = pokemon_count
+
+            with open(VERSION_FILE_PATH, mode ='r') as f:
+                self.version = int(f.read())
+        except:
+            if os.path.exists(CSV_FILE_PATH):
+                os.remove(CSV_FILE_PATH)
+            if os.path.exists(VERSION_FILE_PATH):
+                os.remove(VERSION_FILE_PATH)
+            self.version = -1
+            return
+
+    def retrieve_and_write(self):
+        """
+        Grabs data from oneline websites and encapsulates important
+        data into a csv data table
+        """
+        self._get_data()
+
+        if os.path.exists(CSV_FILE_PATH):
+            os.remove(CSV_FILE_PATH)
+
+        if os.path.exists(VERSION_FILE_PATH):
+            os.remove(VERSION_FILE_PATH)
+
+        with open(CSV_FILE_PATH, 'w') as f:
+            writer = csv.DictWriter(f, fieldnames=CSV_FIELDS)
+            writer.writeheader()
+            writer.writerows(self.csv_data)
+
+        with open(VERSION_FILE_PATH, 'w') as f:
+            f.write(str(CURRENT_VERSION))
+
+        # TODO: Come up with a better fix
+        # This is gross but data must be loaded up the same way everytime to resolve consistency issues
+        self.load()
+
+    def is_current_version(self) -> bool:
+        return self.version == CURRENT_VERSION
+
+    def _get_data(self):
+        pokemon_data = []
+
+        # Start by grabbing special pokemon speech data from a specific website since it will be needed later
+        bs4_filter = bs4.SoupStrainer("ul")
+        loader = WebBaseLoader(
+            web_path=(PKMN_TALK_WEB_PATH),
+            bs_kwargs={"parse_only": bs4_filter}
+        )
+        # Due to how this data is currently retrieved, oddly-specific storing and spliting are needed to ensure proper labeling
+        pkmn_talk_web_data = str(loader.scrape().contents[4])
+        talk_data_list = []
+        # Need to retrieve only actual text from site
+        for entry in pkmn_talk_web_data.split("<li>")[1:]:
+            # Start by removing titled movies/episodes to avoid mislabeling pokemon speech
+            start_char_idx = entry.find("<i>")
+            while start_char_idx != -1:
+                end_char_idx = entry.find("</i>")
+                entry = entry[:start_char_idx] + entry[end_char_idx+4:]
+                start_char_idx = entry.find("<i>")
+
+            # Now remove web chars so only actual text remains
+            start_char_idx = entry.find("<")
+            while start_char_idx != -1:
+                end_char_idx = entry.find(">")
+                entry = entry[:start_char_idx] + entry[end_char_idx+1:]
+                start_char_idx = entry.find("<")
+            talk_data_list.append(entry.lower())
+
+        # Next, get list of all current pokemon from the online pokemondb database
+        # website and filter out website content to just pokemon names and types
+        class_types = ["itype " + x for x in POKEMON_TYPES]
+        class_types.append("ent-name")
+        class_types.append("infocard-list infocard-list-pkmn-lg")
+        bs4_filter = bs4.SoupStrainer(class_=(class_types))
+        loader = WebBaseLoader(
+            web_path=(PKMN_DB_WEB_PATH),
+            bs_kwargs={"parse_only": bs4_filter}
+        )
+        pkmndb_web_data = loader.scrape()
+    
+        # Now sort through data and store pokemon ids, names, types, speech patterns,
+        # generations & urls from retrieving additional information
+        id = 0
+        gen = 0
+        # Due to how this website is specifically parsed, the pokemon content is
+        # grouped by generation and thus each section also needs to be iterated through
+        for data in pkmndb_web_data.contents:
+            gen += 1
+            str_data = str(data)
+            pokemon_list_data= str_data.split(SPLIT_PKMN_LIST_STR)[1:]
+            
+            for curr_pokemon_data in pokemon_list_data:
+                id += 1
+                # Added "[0]" at the end so it returns a string instead of a list with a single, string entry
+                parsed_data = curr_pokemon_data.split(SPLIT_PKMN_DATA_STR)[-1:][0]
+            
+                # All parsed data here has the exact same format:
+                # [child_url]">'[pokemon_name]...<a class="itype [pokemon_type1]"...
+                # (optional): <a class="itype [pokemon_type2]"
+                
+                # Start by extracting child_url
+                end_idx = parsed_data.find("\">")
+                pokemon_url = parsed_data[:end_idx]
+                parsed_data = parsed_data[end_idx+2:]
+                
+                # Now grab pokemon_name
+                end_idx = parsed_data.find("<")
+                pokemon_name = parsed_data[:end_idx]
+                parsed_data = parsed_data[end_idx:]
+                
+                # Then grab types from remaining data
+                parsed_type_data = parsed_data.split(SPLIT_PKMN_TYPE_STR)[1:]
+                end_idx = parsed_type_data[0].find("\"")
+                pokemon_type = parsed_type_data[0][:end_idx]
+                # Some pokemon are dual type, format it as "type1/type2"
+                if (len(parsed_type_data) == 2):
+                    end_idx = parsed_type_data[1].find("\"")
+                    pokemon_type += "/" + parsed_type_data[1][:end_idx]
+                    
+                # Make sure pokemon types are valid
+                list_types = pokemon_type.split("/")
+                for type in list_types:
+                    if type.lower() not in POKEMON_TYPES:
+                        raise Exception(f"{type} is not a valid type.")
+                
+                # Lastly, use talking pokemon data to determine speech attribute
+                # Due to how this data is currently parsed, oddly-specific conditions are needed to ensure proper labeling
+                pokemon_speech = "noise"
+                pokemon_name_lowercase = pokemon_name.lower()
+
+                # Specifically skip Mew since it otherwise gets mislabeled with Mewtwo's speech value
+                if pokemon_name_lowercase == "mew":
+                    continue
+
+                # If current pokemon is mentioned in talk data, find it's correct speech value
+                for talk_data in talk_data_list:
+                    if pokemon_name_lowercase in talk_data:
+                        # Edge Case: some talking pokemon have "without telepathy" in their description
+                        if "without telepathy" in talk_data:
+                            pokemon_speech = "talk"
+
+                        # Any mentions of "telepath" means the pokemon speaks through telepathy
+                        # Edge Case: Sometimes telepathy is implied with "talk through meowth"
+                        elif ("telepath" in talk_data or "talk through meowth" in talk_data) and pokemon_speech != "talk":
+                            pokemon_speech = "telepathy"
+
+                        # If there's no mention of "telepathy", then the pokemon can actually speak
+                        else:
+                            pokemon_speech = "talk"
+
+                # Now package it into a nice dict and append to pokemon list
+                dict_data = {
+                    CSV_ID_KEY:id,
+                    CSV_NAME_KEY:pokemon_name,
+                    CSV_TYPE_KEY:pokemon_type,
+                    CSV_SPEECH_KEY:pokemon_speech,
+                    CSV_GEN_KEY:gen,
+                    CSV_LINKS_KEY:pokemon_name_entry(pokemon_url,id)
+                }
+                pokemon_data.append(dict_data)
+
+        self.csv_data = pokemon_data
+        self.max_pokemon = id
+        self.version = CURRENT_VERSION
+
+
+def pokemon_name_entry(name: str, id: int) -> list:
+    websites = []
+    str_num = str(id)
+    
+    # A formatted string should be 4 digits
+    str_num_formmatted = str_num
+    for i in range(4 - len(str_num)):
+        str_num_formmatted = "0" + str_num_formmatted
+
+    # Append these different pokemon database sites
+    websites.append("https://pokemondb.net/pokedex/" + name)
+    websites.append("https://pokedex.org/#/pokemon/" + str_num)
+    websites.append("https://sg.portal-pokemon.com/play/pokedex/" + str_num_formmatted)
+
+    return websites
+
+
+# Keeping this just in case it's needed later
+def pokemon_type_entry(type: str) -> str:
+    return "https://pokemondb.net/type/" + type
